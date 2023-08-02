864c558743749ace23d38e140d7773fc2aca4017
Bruno Virginio <bruno.virginio23@gmail.com>
2023-08-02 19:33:25 -0300

    feat: Add user notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,52 +6,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  readonly initialSeconds = 1500 * 1000;
+  readonly initialSeconds = 1500;
   formattedTimer = "";
-  interval: any;
-  buttonText = 'Start!';
-  timer = new Date(this.initialSeconds);
+  interval: number | undefined;
+  buttonText = "Start!";
+  timer = this.initialSeconds;
 
   ngOnInit(): void {
-    this.updateFormattedTimer(this.timer);
+    this.updateFormattedTimer();
   }
 
   togglePomodoro() {
-    if (this.interval === undefined) {
-      this.buttonText = "Stop";
-      this.interval = setInterval(() => {
-        let minutes = this.timer.getMinutes();
-        let seconds = this.timer.getSeconds();
-
-        if(minutes == 0 && seconds == 0) {
-          this.resetTimer();
-          alert("Congratulations! Your pomodoro session has finished!");
-          return;
-        }
-
-        this.timer.setSeconds(seconds - 1);
-        this.updateFormattedTimer(this.timer);
-      }, 1000)
-    }
-    else {
-      this.buttonText = "Start!";
-      clearInterval(this.interval);
-      this.interval = undefined;
-    }
+    if (this.interval)
+      this.stopTimer();
+    else
+      this.startTimer();
   }
 
-  resetTimer() {
+  private stopTimer() {
     this.buttonText = "Start!";
-    this.timer = new Date(this.initialSeconds);
-    this.updateFormattedTimer(this.timer);
     clearInterval(this.interval);
     this.interval = undefined;
   }
 
-  updateFormattedTimer(timer: Date) {
-    let minutes = timer.getMinutes() < 10 ? "0" + timer.getMinutes() : timer.getMinutes();
-    let seconds = timer.getSeconds() < 10 ? "0" + timer.getSeconds() : timer.getSeconds();
+  private startTimer() {
+    this.requestNotificationPermission();
+    this.buttonText = "Stop";
+    this.interval = setInterval(() => {
+      if (this.timer == 0) {
+        this.resetTimer();
+        this.showNotification();
+        return;
+      }
 
-    this.formattedTimer = `${minutes}:${seconds}`;
+      this.timer--;
+      this.updateFormattedTimer();
+    }, 1000)
+  }
+
+  resetTimer() {
+    this.buttonText = "Start!";
+    this.timer = this.initialSeconds;
+    this.updateFormattedTimer();
+    clearInterval(this.interval);
+    this.interval = undefined;
+  }
+
+  updateFormattedTimer() {
+      let minutes = Math.floor(this.timer / 60);
+      let seconds = this.timer % 60;
+      this.formattedTimer = `${this.pad(minutes)}:${this.pad(seconds)}`;
+  }
+
+  pad(num: number): string {
+      return num < 10 ? '0' + num : '' + num;
+  }
+
+  requestNotificationPermission() {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          console.log("Notification permission granted.");
+        }
+      });
+    }
+  }
+
+  showNotification() {
+    if (Notification.permission === "granted") {
+      new Notification("Pomodoro Session", {
+        body: "Congratulations! Your pomodoro has finished.",
+        icon: "/assets/logo.png", // Replace with your own icon URL
+      });
+    }
   }
 }
